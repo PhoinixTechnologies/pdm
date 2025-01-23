f4a364fcd09be0eda50adbcc6266a1dc4af67504
Praisebuka <praise.njoga@gmail.com>
2025-01-23 20:27:42 +0100

    added dashboard

---
+++ app/dashboard/page.js
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+import './dashboard.styles.scss';
+import Link from 'next/link';
+import logo_icon from '../../public/assets/icons/pdm.png';
+
+
+
+export default function Dashboard() {
+
+  return (
+    <div className="pdm-dashboard">
+      Dashboard starts here
+    </div> 
+  );
+}
