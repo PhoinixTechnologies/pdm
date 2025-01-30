d7dee6b632d359c61da85ad21da563bc70d0bfc9
Praisebuka <praise.njoga@gmail.com>
2025-01-31 00:48:38 +0100

    ok, good.... I need to sleep

--- app/course/watch/[id]/page.js
+++ app/course/watch/[id]/page.js
@@ -2,10 +2,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './watch-course.scss';
 import DashboardHeader from '@/components/dashboard/dash.header';
 import { faArrowLeft, faClock, faFolder, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import avatarOne from '../../../../public/assets/images/course/watch/avatar_one.png';
+import avatarTwo from '../../../../public/assets/images/course/watch/avatar_two.png';
+import avatarThree from '../../../../public/assets/images/course/watch/avatar_three.png';
+import avatarFour from '../../../../public/assets/images/course/watch/avatar_four.png';
+import avatarFive from '../../../../public/assets/images/course/watch/avatar_five.png';
 
 
 
 export default function WatchCourse() {
+
+  const date = Date().split(" ").slice(1, 4).join(" ");
 
   return (
       <div className="pdm">
@@ -33,7 +41,32 @@ export default function WatchCourse() {
           </div>
 
           <div className='bottom'>
+            <div className='video-title'>
+              <iframe width="950" height="650" src="https://www.youtube.com/embed/F4Zu5ZZAG7I?si=jd9LC5lAry_AKo0Z" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerPolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
 
+              <h3> 7 Ways to Make a Conversation With Anyone | Malavika Varadan </h3>
+
+              <div className='statistics'> 
+                <div className='left'>
+                  <div className='icons'>
+                    <Image src={avatarOne} alt='profile icon' width={100} height={100} />
+                    <Image src={avatarTwo} alt='profile icon' width={100} height={100} />
+                    <Image src={avatarThree} alt='profile icon' width={100} height={100} />
+                    <Image src={avatarFour} alt='profile icon' width={100} height={100} />
+                    <Image src={avatarFive} alt='profile icon' width={100} height={100} />
+                  </div>
+                  <div className='photos'>
+                    <h6> 512 </h6>
+                    <p> Students watching </p>
+                  </div>
+                </div>
+
+                <div className='right'>
+                  <p> Last updated: <b> {date} </b> </p>
+                  <p> Comments: <b> 154 </b> </p>
+                </div>
+              </div>
+            </div>
           </div>
         </main>
       </div> 
