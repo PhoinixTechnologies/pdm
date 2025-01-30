323f6c7c1b0b777efb73766f60ec23256dd3d42c
Praisebuka <praise.njoga@gmail.com>
2025-01-30 23:25:48 +0100

    header top configed

--- app/course/watch/[id]/page.js
+++ app/course/watch/[id]/page.js
@@ -1,5 +1,7 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './watch-course.scss';
 import DashboardHeader from '@/components/dashboard/dash.header';
+import { faArrowLeft, faClock, faFolder, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
 
 
 
@@ -10,7 +12,29 @@ export default function WatchCourse() {
         <DashboardHeader />
       
         <main className='watch-courses'>
-          <p> Watch Courses Section starts here </p>
+          <div className='top'>
+            <button type='button'> <FontAwesomeIcon icon={faArrowLeft} /> </button>
+
+            <div className='middle'>
+              <div className='details'>
+                <h5> Python Programming </h5>
+                <div>
+                  <p> <FontAwesomeIcon icon={faFolder} /> 6 Sections </p>
+                  <p> <FontAwesomeIcon icon={faPlayCircle} /> 202 Lectures </p>
+                  <p> <FontAwesomeIcon icon={faClock} /> 19h 37m </p>
+                </div>
+              </div>
+
+              <div className='btns'>
+                <button type='button'> Write A Review </button>
+                <button type='button'> Next Lecture </button>
+              </div>
+            </div>
+          </div>
+
+          <div className='bottom'>
+
+          </div>
         </main>
       </div> 
   );
