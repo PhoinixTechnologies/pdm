a581e2fcb1207f7d5aff2a4fa9e9b8f255ca4649
Praisebuka <praise.njoga@gmail.com>
2025-02-04 17:05:41 +0100

    I think I'm done here :)

--- app/course/watch/[id]/page.js
+++ app/course/watch/[id]/page.js
@@ -1,13 +1,14 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import './watch-course.scss';
 import DashboardHeader from '@/components/dashboard/dash.header';
-import { faArrowLeft, faClock, faFolder, faPlayCircle } from '@fortawesome/free-solid-svg-icons';
+import { faArrowLeft, faClock, faComment, faDownload, faFolder, faPlayCircle, faSpinner } from '@fortawesome/free-solid-svg-icons';
 import Image from 'next/image';
 import avatarOne from '../../../../public/assets/images/course/watch/avatar_one.png';
 import avatarTwo from '../../../../public/assets/images/course/watch/avatar_two.png';
 import avatarThree from '../../../../public/assets/images/course/watch/avatar_three.png';
 import avatarFour from '../../../../public/assets/images/course/watch/avatar_four.png';
 import avatarFive from '../../../../public/assets/images/course/watch/avatar_five.png';
+import { notificationAlert } from '@/utils/utils';
 
 
 
@@ -70,8 +71,74 @@ export default function WatchCourse() {
 
             <section className='rest'>
               <div className='rest-btns'>
-                <button type='button'> Description </button>
+                <button type='button' className='description-btn'> Description </button>
+                <button type='button' className=''> Lectures Notes </button>
+                <button type='button' className=''> Attach File <span> 01 </span> </button>
+                <button type='button' className=''> Comments </button>
               </div>
+
+              <div className='description'>
+                <div>
+                  <h3> Lecture Description </h3>
+                  <p> This Python Programming course covers everything from basic concepts like Python installation, syntax, variables, and data structures (strings, lists, tuples, sets, and dictionaries) to intermediate topics such as control flow, functions, modules, and file handling, and finally advanced subjects like object-oriented programming (OOP), web scraping, GUI creation, working with databases, regular expressions, network programming, and web development with Flask, making it perfect for beginners, intermediate learners, aspiring data scientists, web developers, IT professionals, and hobbyists, with no prior programming experience required beyond basic computer literacy. </p>
+                </div>
+                <div>
+                  <h3> Lectures Notes <button onClick={notificationAlert('success', 'Congratulations!', 'Hi there, your download process have started :)')} type='button'> <FontAwesomeIcon icon={faDownload} /> Download Notes </button> </h3>
+                  <p> The Python Programming course lecture notes cover the basics of Python, including installation, variables, data types, operators, input/output, and core data structures (strings, lists, tuples, sets, dictionaries), progressing through intermediate topics like control flow (if-else, loops), functions, and modules, to file handling, equipping students with a solid foundation for more advanced programming concepts. </p>
+                </div>
+              </div>
+
+              <div className='comments'>
+                <h3> Comments (154) </h3>
+
+                <div className='combine'>
+                  <Image src={avatarFive} alt='progfile icon' />
+                  <form action={'#'} method='POST'>
+                    <h6> Ronald Richards <span> . </span> <small> 1 week ago </small> </h6>
+                    <p> Thank You so much ma, you’re a great mentor. 🔥🔥🔥 </p>
+                    <input type='text' name='comment' placeholder='💬 REPLY...'  />
+                  </form>                  
+                </div>
+
+                <div className='combine'>
+                  <Image src={avatarThree} alt='progfile icon' />
+                  <form action={'#'} method='POST'>
+                    <h6> Guy Hawkins <span> . </span> <small> 2 week ago </small> </h6>
+                    <p> Thank you for your helpful video.  </p>
+                    <input type='text' name='comment' placeholder='💬 REPLY...'  />
+                  </form>                  
+                </div>
+
+                <div className='combine'>
+                  <Image src={avatarOne} alt='progfile icon' />
+                  <form action={'#'} method='POST'>
+                    <h6> Esther Howard <span> . </span> <small> 2 week ago </small> </h6>
+                    <p> Quality Content </p>
+                    <input type='text' name='comment' placeholder='💬 REPLY...'  />
+                  </form>                  
+                </div>
+
+                <div className='combine'>
+                  <Image src={avatarTwo} alt='progfile icon' />
+                  <form action={'#'} method='POST'>
+                    <h6> Theresa Webb <span> . </span> <small> 3 week ago </small> </h6>
+                    <p> Now i know that i will spent that 5 minutes of my life with pure pleasure </p>
+                    <input type='text' name='comment' placeholder='💬 REPLY...'  />
+                  </form>                  
+                </div>
+
+                <div className='combine'>
+                  <Image src={avatarThree} alt='progfile icon' />
+                  <form action={'#'} method='POST'>
+                    <h6> Courtney Henry <span> . </span> <small> 1 month ago </small> </h6>
+                    <p> Imagine seeing this while being a front end programmer. </p>
+                    <input type='text' name='comment' placeholder='💬 REPLY...'  />
+                  </form>                  
+                </div>
+              </div>
+
+              <button type='button' id='load-more'> Load more <FontAwesomeIcon icon={faSpinner} /> </button>
+
             </section>
           </div>
         </main>
