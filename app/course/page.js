365c5a2835cea4614c9c376f03ff8e8c2362163a
Praisebuka <praise.njoga@gmail.com>
2025-02-09 22:56:19 +0100

    PC's down, will continue later..

--- app/course/page.js
+++ app/course/page.js
@@ -2,6 +2,8 @@ import './course.styles.scss';
 import Link from 'next/link';
 import DashboardHeader from '@/components/dashboard/dash.header';
 import Head from 'next/head';
+import Image from 'next/image';
+import courseOnePreview from '../../public/assets/images/course/Course Images1.png'
 
 
 
@@ -15,7 +17,22 @@ export default function Course() {
 
         <DashboardHeader />
       
-        
+        <section className='both'>
+          <div className='main'>
+            <div className='each'>
+              <Image src={courseOnePreview} alt='course icon preview logo' />
+
+              <div>
+                <p> IT & Software <span> #50,000 </span> </p>
+                <h5> Learn Python Programming Masterclass </h5>
+              </div>
+            </div>
+          </div>
+
+          <div className='summary'>
+
+          </div>
+        </section>
       </div> 
   );
 }
