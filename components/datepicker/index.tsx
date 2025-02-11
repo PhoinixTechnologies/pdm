72455fbd87ba7adc0422d4cd079edc1fcdcd93eb
Praisebuka <praise.njoga@gmail.com>
2025-02-12 00:32:54 +0100

    date picker's good to go

--- components/datepicker/index.tsx
+++ components/datepicker/index.tsx
@@ -1,9 +1,11 @@
 "use client"
-import './style.css'
-import React, { useState } from "react";
+import './style.scss'
+import React, { useEffect, useRef, useState } from "react";
 import dayjs from "dayjs";
 import { LeftOutlined, RightOutlined } from "@ant-design/icons";
 import { useSwipeable } from "react-swipeable";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faClock, faEllipsis, faTowerCell } from '@fortawesome/free-solid-svg-icons';
 
 const DatePicker = () => {
   const today = dayjs();
@@ -34,30 +36,81 @@ const DatePicker = () => {
     onSwipedRight: handlePrev,
   });
 
+
+  
+  
+
   return (
-    <div className="date-picker-container" {...swipeHandlers}>
-      <button className="nav-button" onClick={handlePrev}>
-        <LeftOutlined />
-      </button>
-      <div className="date-list">
-        {dates.map((date) => (
-          <div
-            key={date.format("DD-MM-YYYY")}
-            className={`date-item ${
-              date.isSame(selectedDate, "day") ? "selected" : ""
-            }`}
-            onClick={() => setSelectedDate(date)}
-          >
-            <span className="day">{date.format("DD")}</span>
-            <span className="weekday">{date.format("ddd")}</span>
-            {date.isSame(today, "day") && <div className="dot"></div>}
-          </div>
-        ))}
-      </div>
-      <button className="nav-button" onClick={handleNext}>
-        <RightOutlined />
-      </button>
-    </div>
+        <div className='summary'>
+            <div className="date-picker-container" {...swipeHandlers}>
+                <div className='cursors-nd-month'>
+                    <button className="nav-button" onClick={handlePrev}> <LeftOutlined /> </button>
+                    <p> {dates[0].format("MMMM YYYY")} </p>
+                    <button className="nav-button" onClick={handleNext}> <RightOutlined /> </button>
+                </div>
+
+                <div className="date-list">
+                    {dates.map((date, index) => {
+                        const isDot2 = index >= 2 && index <= 4;
+                        return (
+                        <div  key={date.format("DD-MM-YYYY")}  className={`date-item ${date.isSame(selectedDate, "day") ? "selected" : ""}`}  onClick={() => setSelectedDate(date)}>
+                            <span className="day"> {date.format("DD")} </span>
+                            <span className="weekday"> {date.format("ddd")} </span>
+                            {date.isSame(today, "day") && <div className="dot"></div>}
+                            {isDot2 && <div className="dot-two"></div>}
+                        </div>
+                        );
+                    })}
+                </div>
+            </div>
+
+            <div className='reviews'>
+                <div className='each-item'>
+                    <div>
+                    <p> <FontAwesomeIcon icon={faClock} /> 10:00 - 10:30 </p>
+                    <button type='button'> <FontAwesomeIcon icon={faEllipsis} /> </button>
+                    </div>
+
+                    <h6> Works review </h6>
+                    <em> Data ANayltics </em>
+                </div>
+                
+                <div className='each-item live'>
+                    <div>
+                    <p> <FontAwesomeIcon icon={faClock} /> 11:00 - 12:30 </p>
+                    <button type='button'> <FontAwesomeIcon icon={faEllipsis} /> </button>
+                    </div>
+
+                    <div>
+                    <FontAwesomeIcon icon={faTowerCell} />
+                    <div>
+                        <h6> Notes and messages </h6>
+                        <em> Variables </em>
+                    </div>
+                    </div>
+                </div>
+
+                <div className='each-item'>
+                    <div>
+                    <p> <FontAwesomeIcon icon={faClock} /> 15:00 - 16:45 </p>
+                    <button type='button'> <FontAwesomeIcon icon={faEllipsis} /> </button>
+                    </div>
+
+                    <h6> Teamwork </h6>
+                    <em> Python Fundamentals </em>
+                </div>
+
+                <div className='each-item'>
+                    <div>
+                    <p> <FontAwesomeIcon icon={faClock} style={{ color: '#CDDC39' }} /> 17:00 - 17:30 </p>
+                    <button type='button'> <FontAwesomeIcon icon={faEllipsis} /> </button>
+                    </div>
+
+                    <h6> Tutor </h6>
+                </div>
+
+            </div>
+        </div>
   );
 };
 
