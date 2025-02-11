a5a097e104c6be40ba7a9734acf4fa85c4fb75db
Praisebuka <praise.njoga@gmail.com>
2025-02-11 22:40:12 +0100

    I think I shld check antd before proceeding

--- app/course/page.js
+++ app/course/page.js
@@ -8,7 +8,8 @@ import courseTwoPreview from '../../public/assets/images/course/Image2.png'
 import courseThreePreview from '../../public/assets/images/course/Image3.png'
 import courseFourPreview from '../../public/assets/images/course/Image4.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faClock, faListDots, faStar, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faIntercom } from '@fortawesome/free-brands-svg-icons';
 
 
 
@@ -94,7 +95,56 @@ export default function Course() {
           </div>
 
           <div className='summary'>
+            <div className='dates'>
 
+            </div>
+
+            <div className='reviews'>
+              <div className='each-item'>
+                <div>
+                  <p> <FontAwesomeIcon icon={faClock} /> 10:00 - 10:30 </p>
+                  <button type='button'> <FontAwesomeIcon icon={faListDots} /> </button>
+                </div>
+
+                <h6> Works review </h6>
+                <em> Data ANayltics </em>
+              </div>
+              
+              <div className='each-item live'>
+                <div>
+                  <p> <FontAwesomeIcon icon={faClock} /> 11:00 - 12:30 </p>
+                  <button type='button'> <FontAwesomeIcon icon={faListDots} /> </button>
+                </div>
+
+                <div>
+                  <FontAwesomeIcon icon={faIntercom} />
+                  <div>
+                    <h6> Notes and messages </h6>
+                    <em> Variables </em>
+                  </div>
+                </div>
+              </div>
+
+              <div className='each-item'>
+                <div>
+                  <p> <FontAwesomeIcon icon={faClock} /> 15:00 - 16:45 </p>
+                  <button type='button'> <FontAwesomeIcon icon={faListDots} /> </button>
+                </div>
+
+                <h6> Teamwork </h6>
+                <em> Python Fundamentals </em>
+              </div>
+
+              <div className='each-item'>
+                <div>
+                  <p> <FontAwesomeIcon icon={faClock} style={{ color: '#CDDC39' }} /> 17:00 - 17:30 </p>
+                  <button type='button'> <FontAwesomeIcon icon={faListDots} /> </button>
+                </div>
+
+                <h6> Tutor </h6>
+              </div>
+
+            </div>
           </div>
         </section>
       </div> 
