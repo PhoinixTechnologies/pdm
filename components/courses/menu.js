8ee4dc041779d577be91dd3aaae49218cb7db7f1
Praisebuka <praise.njoga@gmail.com>
2025-02-19 15:10:15 +0100

    more dynamicity I guess :)

--- components/courses/menu.js
+++ components/courses/menu.js
@@ -8,6 +8,7 @@ import { faClock, faFileShield, faPlay, faPlayCircle } from '@fortawesome/free-s
 
 export default function ClientMenu() {
   let keyValue = 1;
+  let subInt = 1;
   const [selectedKey, setSelectedKey] = useState(null);
 
   const handleSelect = ({ key }) => {
@@ -39,8 +40,8 @@ export default function ClientMenu() {
 
   const items = [
     {
-      key: 'sub1',
-      label: renderLabel('Introduction to Python', 'sub1', '8 lectures - 51m'),
+      key: `sub ${subInt++}`,
+      label: renderLabel('Introduction to Python', `sub ${subInt++}`, '8 lectures - 51m'),
       children: [
         {
           key: 'g1',
@@ -60,24 +61,142 @@ export default function ClientMenu() {
       ],
     },
     {
-      key: 'sub2',
-      label: renderLabel('Python Basics', 'sub2', '52 lectures - 5h 49m'),
+      key: `sub ${subInt++}`,
+      label: renderLabel('Python Basics', `sub ${subInt++}`, '52 lectures - 5h 49m'),
       children: [
         { key: `${keyValue++}`, label: renderLabel('Python Basics run-down', `${keyValue++}`) },
       ],
     },
     {
-      key: 'sub3',
-      label: renderLabel('Data Structures (Strings)', 'sub3', '52 lectures - 5h 49m'),
+      key: `sub ${subInt++}`,
+      label: renderLabel('Data Structures (Strings)', `sub ${subInt++}`, '52 lectures - 5h 49m'),
       children: [
         { key: `${keyValue++}`, label: renderLabel('Understanding Strings', `${keyValue++}`) },
       ],
     },
     {
-      key: 'sub4',
-      label: renderLabel('Data Structure (List)', 'sub4', '52 lectures - 5h 49m'),
+      key: `sub ${subInt++}`,
+      label: renderLabel('Data Structure (List)', `sub ${subInt++}`, '52 lectures - 5h 49m'),
       children: [
         { key: `${keyValue++}`, label: renderLabel('Working with Lists', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Data Structures(Tuples)', `sub ${subInt++}`, '52 lectures - 5h 49m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Working with Tuples', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Data Structure (Sets)', `sub ${subInt++}`, '52 lectures - 5h 49m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Working with Sets', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Data Structure (Dictionaries)', `sub ${subInt++}`, '52 lectures - 5h 49m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Working with Dictionaries', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Summary of Data Structures:', `sub ${subInt++}`, '52 lectures - 5h 49m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Summary of Data Structures:', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Program Flow(Control Flow Statements):', `sub ${subInt++}`, '52 lectures - 5h 49m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Program Flow(Control Flow Statements):', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Functions & Modules:', `sub ${subInt++}`, '43 lectures - 53m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Program Flow(Control Flow Statements):', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('File Handling:', `sub ${subInt++}`, '137 lectures - 10h 6m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('File Handling:', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('CSV & JSON Files:', `sub ${subInt++}`, '21 lectures - 38m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('CSV & JSON Files:', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Word, PDF & JSON Documents using Python:', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Word, PDF & JSON Documents using Python:', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Handling Excel Files usinf Python:', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Handling Excel Files usinf Python:', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Classes & OOP:', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Classes & OOP:', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Web Scraping:', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Web Scraping:', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Writing GUIs in Python(The Graphical User Interface Application):', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Writing GUIs in Python(The Graphical User Interface Application):', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Regular Expression:', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Regular Expression:', `${keyValue++}`) },
+      ],
+    },{
+      key: `sub ${subInt++}`,
+      label: renderLabel('Network Programming:', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Network Programming:', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('Flask', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('Flask', `${keyValue++}`) },
+      ],
+    },
+    {
+      key: `sub ${subInt++}`,
+      label: renderLabel('MySQL Database in Python:', `sub ${subInt++}`, '39 lectures - 91m'),
+      children: [
+        { key: `${keyValue++}`, label: renderLabel('MySQL Database in Python:', `${keyValue++}`) },
       ],
     },
   ];
