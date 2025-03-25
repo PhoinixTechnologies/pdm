946b4fd2391ed361d9b1d9c477699e778cdc595c
Praisebuka <praise.njoga@gmail.com>
2025-03-25 10:52:44 +0100

    there shld be 404

--- src/router/route.js
+++ src/router/route.js
@@ -4,7 +4,7 @@ import { useAuth } from "../hook/AuthProvider";
 
 const PrivateRoute = () => {
     const user = useAuth();
-    if (!user.token) return <Navigate to="/" />;
+    if (!user.token) return <Navigate to="/login" />; // Should return to 404 page....will implement later on
     return <Outlet />;
 };
 
