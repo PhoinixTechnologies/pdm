49f980e0ef470937ffb5dca5f65541cf1bda71a8
Praisebuka <praise.njoga@gmail.com>
2025-03-29 09:51:01 +0100

    this isn't still the cause :(

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,12 +3,12 @@ import Swal from "sweetalert2";
 
 export function notificationAlert(icon, title, text) {
 
-    Swal.fire({ icon, title, text });
-  
-    if (!icon || !title || !text) {
-      Swal.fire({ icon: 'error', title: 'Try Again', text: 'Required params not found'});
-      console.log("Required params not found");
-    }
+  if (!icon || !title || !text) {
+    Swal.fire({ icon: 'error', title: 'Try Again', text: 'Required params not found'});
+    console.log("Required params not found");
+  }
+
+  Swal.fire({ icon: icon, title: title, text: text });
   
 }
 
