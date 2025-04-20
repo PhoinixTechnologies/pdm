20ab4150b06d336da6af748738366e82b9827b8b
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-04-20 20:18:51 +0100

    card radius adjusted

--- src/aboutus.jsx
+++ src/aboutus.jsx
@@ -58,8 +58,8 @@ const About = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="185" height="21" viewBox="0 0 185 21" fill="none">
                 <path d="M0 10C0 4.47715 4.47715 0 10 0H156.429C161.902 0 167.136 2.2433 170.911 6.2069L185 21H0V10Z" fill="#504E52"/>
                 </svg>
-                <div className="card-bg bg-[url('./img/Python.png')]">
-                    <div className="card-gradient bg-gradient-to-b from-top/100 via-mid/40 to-black"></div>
+                <div className="card-bg bg-[url('./img/Python.png')]  rounded-tr-4xl">
+                    <div className="card-gradient bg-gradient-to-b from-top/100 via-mid/40 to-black  rounded-tr-4xl"></div>
                     <div className="card-content">
                         <h1 className="text-2xl font-bold tracking-wider font-sora">Python Programming</h1>
                         <p className='pText'> Key Python concepts include variables, control flow, functions, data structures, object-oriented programming, and error handling.</p>
@@ -72,8 +72,8 @@ const About = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="185" height="21" viewBox="0 0 185 21" fill="none">
                 <path d="M0 10C0 4.47715 4.47715 0 10 0H156.429C161.902 0 167.136 2.2433 170.911 6.2069L185 21H0V10Z" fill="#D0D0EC"/>
                 </svg>
-                <div className="card-bg   bg-[url('./img/ml.png')]">
-                <div className="card-gradient bg-gradient-to-b from-top2/80 via-mid/30 to-black/70  "></div>
+                <div className="card-bg   bg-[url('./img/ml.png')]  rounded-r-4xl rounded-bl-4xl">
+                <div className="card-gradient bg-gradient-to-b from-top2/80 via-mid/30 to-black/70 "></div>
                 <div className="card-content">
                         <h1 className="text-2xl font-bold tracking-wider font-sora">Data Analytics</h1>
                         <p className='pText'> Key data analytics concepts include data collection, cleaning, exploratory analysis, visualization, statistical modeling, and deriving actionable insights.</p>
@@ -85,8 +85,8 @@ const About = () => {
                 <svg xmlns="http://www.w3.org/2000/svg" width="185" height="21" viewBox="0 0 185 21" fill="none">
                 <path d="M0 10C0 4.47715 4.47715 0 10 0H156.429C161.902 0 167.136 2.2433 170.911 6.2069L185 21H0V10Z" fill="#B5C0DC"/>
                 </svg>
-                <div className=" card-bg bg-[url('./img/DA.png')] ">
-                <div className=" bg-gradient-to-b from-top3/90 via-mid/30 to-black/40 card-gradient"></div>
+                <div className=" card-bg bg-[url('./img/DA.png')] rounded-r-4xl rounded-bl-4xl">
+                <div className=" bg-gradient-to-b from-top3/90 via-mid/30 to-black/40 card-gradient "></div>
                 <div className="card-content">
                         <h1 className="text-2xl font-bold tracking-wider font-sora">Machine Learning</h1>
                         <p className='pText'> Key machine learning concepts include data preprocessing, feature engineering, model selection, training, evaluation, and tuning for accurate predictions.</p>
