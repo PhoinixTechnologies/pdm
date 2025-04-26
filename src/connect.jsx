96751ef910263a93a5dc29dac5fcdc327ad30c3e
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-04-26 10:06:58 +0100

    mobile view completed

--- src/connect.jsx
+++ src/connect.jsx
@@ -2,23 +2,23 @@
 import Plogo from './img/Plogo.png'
 const Connect = () => {
     return (
-        <div className=' w-full bg-black pt-10 flex flex-col gap-y-5 border border-white h-max' id='connect'>
-             <button type="button" className='input-btn place-self-center border border-white'>
+        <div className=' w-full bg-black pt-10 flex flex-col gap-y-5 h-max' id='connect'>
+             <button type="button" className='input-btn place-self-center'>
                 <p className='input-text text-white'>
                     Connect with us Today
                 </p>
             </button>
-            <div className='flex w-full gap-x-6 justify-start text-white h-max p-20 py-10 pb-2'>
-                <div className=' w-max pr-10 font-link h-max p-2'>
-                    <p className='font-semibold text-4xl/10 w-90'>
+            <div className='flex w-full gap-x-6 gap-y-10 justify-center text-white h-max md:px-20 py-0 md:py-10 pb-2 flex-col md:flex-row items-center '>
+                <div className=' w-max pr-10 font-link h-max p-0'>
+                    <p className='font-semibold text-6xl md:text-4xl/10 w-90'>
                         We are always  happy to assist you
                     </p>
                 </div>
-                <div className='flex gap-x-[50px]  p-2 font-logo w-[50%]'>
+                <div className='flex flex-col md:flex-row gap-x-[50px]  p-2 font-logo md:w-[50%] w-100 text-xl md:text-sm gap-y-10'>
                     <span className='flex flex-col gap-y-3'>
                         <h1 className='input-text'>Email Address</h1>
                         <div className='border border-white w-5'> </div>
-                        <h1 className='input-text'>mypdmtutor@gmail.com</h1>
+                        <h1 className='input-text tracking-widest'>mypdmtutor@gmail.com</h1>
                        <div className='text-sm'>
                        <p className='font-light'>Assistance hours:</p>
                        <p className='font-light w-40'>Monday - Friday 6am to  8pm EST</p>
@@ -27,7 +27,7 @@ const Connect = () => {
                     <span className='flex flex-col gap-y-3'>
                         <h1 className='input-text'>Number</h1>
                         <div className='border border-white w-5'> </div>
-                        <h1 className='input-text'>+234 803 986 7195</h1>
+                        <h1 className='input-text tracking-widest'>+234 803 986 7195</h1>
                        <div className='text-sm'>
                        <p className='font-light'>Assistance hours:</p>
                        <p className='font-light w-40'>Monday - Friday 6am to  8pm EST</p>
@@ -35,16 +35,16 @@ const Connect = () => {
                     </span>
                 </div>
             </div>
-            <div className='flex w-full gap-x-6 justify-center text-white h-max p-20 py-7  bg-[#2A27C9] items-center flex-col md:flex-row'>
-                <div className=' w-[50%] pr-10 font-link h-max p-2 gap-y-5 flex flex-col'>
-                    <p className='font-semibold text-3xl/10 w-max'>
+            <div className='flex w-full gap-x-6 gap-y-5 justify-center text-white h-max p-20 py-7  bg-[#2A27C9] items-center flex-col md:flex-row'>
+                <div className='w-100 md:w-[50%] pr-10 font-link h-max p-2 gap-y-5 flex flex-col md:items-start items-center'>
+                    <p className='font-semibold text-4xl/15 md:text-3xl/10 w-80 md:w-max'>
                     Subscribe to our Newsletter
                     </p>
-                    <p className=' w-125'>
+                    <p className='w-80 md:w-125 text-xl'>
                     Subscribe for Updates: Stay informed about the python programming, Data Analytics, Machine Learning by subscribing to our newsletter. 
                     </p>
                 </div>
-                <div className='flex p-2 font-logo w-[50%] h-max items-center'>
+                <div className='flex p-2 font-logo w-100 md:w-[50%] h-max items-center'>
                    <input type="email" name="" id="" placeholder='Enter your email address' className='outline-none bg-white/10 pl-5 pr-10 py-5 rounded-l-2xl w-full' />
                    <button className=' px-5 py-5 bg-white text-[#2A27C9] rounded-r-2xl font-semibold shadow-lg shadow-black/10'>Suscribe</button>
                 </div>
