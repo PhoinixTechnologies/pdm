de62b773d75f2521388f58f95b7e7be3b7d74fab
Ife-Banks <ifeoluwa.bankole05@gmail.com>
2025-05-02 00:12:13 +0100

    Join waitlist Almost done

--- src/pages/JoinWait.jsx
+++ src/pages/JoinWait.jsx
@@ -1,34 +1,42 @@
-import logo from './img/My_PDM_Tutor_1-removebg-preview.png';
+import logo from '../img/My_PDM_Tutor_1-removebg-preview.png';
 const Join = () => {
     return ( 
-     <div className=' w-[35%] h-[90%] flex flex-col gap-y-10 items-center py-5'>
-          <div>
-          <img src={logo} alt="" />
-          <h1 className='font-bold font-logo text-xl'>My PDM Tutor</h1>
+     <div className=' w-[40%] h-[100%] flex flex-col gap-y-10 items-center py-10'>
+          <div className='flex gap-x-2 items-center'>
+               <img src={logo} alt="" className='w-10' />
+               <h1 className='font-bold font-logo text-xl'>My PDM Tutor</h1>
           </div>
             
-          <div className=' bg-white w-full  flex flex-col gap-y-10 rounded-xl border-1 border-[#E2E8F0] py-5'>
-                <h1 className='font-bold font-logo text-xl self-center'>Create an account</h1>
-                <form action="" className='p-2 gap-y-10 flex flex-col '>
-                   <div className='flex flex-col px-1 gap-2 border'>
-                        <label htmlFor="">Full Name</label>
-                        <input type="text" className="w-full outline-none bg-transparent  border-2 py-2 px-2 rounded-xl  border-[#9EA2AE]"placeholder="Enter your fullname"/>
+          <div className=' bg-white w-full  flex flex-col gap-y-10 rounded-xl border-1 border-[#E2E8F0] py-5 shadow-[0px_2px_4px_-2px_rgba(0,0,0,0.10),0px_4px_6px_-1px_rgba(0,0,0,0.10)]'>
+                <h1 className='font-semibold font-logo text-2xl self-center'>Create an account</h1>
+                <form action="" className='px-10 gap-y-7 flex flex-col '>
+                   <div className='flex flex-col px-1 gap-2'>
+                        <label htmlFor="" className='font-link text-lg font-medium'>Full Name</label>
+                        <input type="text" className="w-full outline-none bg-transparent  border-2 py-3 px-3 rounded-xl  border-[#9EA2AE]"placeholder="Enter your full name"/>
                    </div>
-                   <div className='flex flex-col px-1 gap-2 border'>
-                        <label htmlFor="">Full Name</label>
-                        <input type="text" className="w-full outline-none bg-transparent  border-2 py-2 px-2 rounded-xl  border-[#9EA2AE]"placeholder="Enter your fullname"/>
+                   <div className='flex flex-col px-1 gap-1'>
+                        <label htmlFor=" " className='font-link text-lg font-medium'>Email Address</label>
+                        <input type="text" className="w-full outline-none bg-transparent  border-2 py-3 px-3 rounded-xl  border-[#9EA2AE]"placeholder="Enter your email address"/>
                    </div>
-                   <div className='flex flex-col px-1 gap-2 border'>
-                        <label htmlFor="">Full Name</label>
-                        <input type="text" className="w-full outline-none bg-transparent  border-2 py-2 px-2 rounded-xl  border-[#9EA2AE]"placeholder="Enter your fullname"/>
+                   <div className='flex flex-col px-1 gap-1'>
+                        <label htmlFor="" className='font-link text-lg font-medium'>Password</label>
+                        <input type='password' className="w-full outline-none bg-transparent  border-2 py-3 px-3 rounded-xl  border-[#9EA2AE]"placeholder="Enter your password"/>
                    </div>
-                   <div className='flex flex-col px-1 gap-2 border'>
-                        <label htmlFor="">Full Name</label>
-                        <input type="text" className="w-full outline-none bg-transparent  border-2 py-2 px-2 rounded-xl  border-[#9EA2AE]"placeholder="Enter your fullname"/>
+                   <div className='flex flex-col px-1 gap-1'>
+                        <label htmlFor="" className='font-link text-lg font-medium'>What do you want to learn?</label>
+                        {/* <input type="text" className="w-full outline-none bg-transparent  border-2 py-4 px-3 rounded-xl  border-[#9EA2AE]"placeholder="Enter your fullname"/> */}
+                        <select name="courses" id="courses" className='w-full outline-none bg-transparent  border-2 rounded-xl py-3 px-3 border-[#9EA2AE]'>
+                        <option value="0">Select Course:</option>
+                         <option value="1">Python Programmming</option>
+                         <option value="2">Machine Learning</option>
+                         <option value="3">Artificial Intelligence</option>
+                        </select>
                    </div>
                    
                 </form>
+                <div></div>
           </div>
+
      </div>
      );
 }
